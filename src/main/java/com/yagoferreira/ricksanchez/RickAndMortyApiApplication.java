473636021fb6ed4d32d4af50a7e94e1c9c2f682a
Yago <yagofer12@gmail.com>
2024-05-16 20:58:49 -0300

    first commit

---
+++ src/main/java/com/yagoferreira/ricksanchez/RickAndMortyApiApplication.java
@@ -0,0 +1,13 @@
+package com.yagoferreira.ricksanchez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickAndMortyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yagoferreira/ricksanchez/RickAndMortyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yagoferreira.ricksanchez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
